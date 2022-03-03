af321f64c27f459105ef44d737d63efc84e0fc29
beniwein <beni_weinmann@hotmail.com>
2022-03-03 14:25:11 +0100

    new-js-file-added

---
+++ conf.js
@@ -0,0 +1,8 @@
+document.getElementById("firstName").innerText = sessionStorage.getItem("firstName");
+document.getElementById("lastName").innerText = sessionStorage.getItem("lastName");
+document.getElementById("address1").innerText = sessionStorage.getItem("address1");
+document.getElementById("address2").innerText = sessionStorage.getItem("address2");
+document.getElementById("plz").innerText = sessionStorage.getItem("plz");
+document.getElementById("city").innerText = sessionStorage.getItem("city");
+document.getElementById("email").innerText = sessionStorage.getItem("email");
+
