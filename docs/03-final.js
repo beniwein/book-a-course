278a6b29b9c833cf2c063e0a682ad8ad02d92059
beniwein <beni_weinmann@hotmail.com>
2022-03-06 13:12:34 +0100

    home-office

--- docs/03-final.js
+++ docs/03-final.js
@@ -1,3 +1,5 @@
+function fillIn() {
+// Personalien - Textblock:
 document.getElementById("firstName").innerText = sessionStorage.getItem("firstName");
 document.getElementById("lastName").innerText = sessionStorage.getItem("lastName");
 document.getElementById("address1").innerText = sessionStorage.getItem("address1");
@@ -5,4 +7,8 @@ document.getElementById("address2").innerText = sessionStorage.getItem("address2
 document.getElementById("plz").innerText = sessionStorage.getItem("plz");
 document.getElementById("city").innerText = sessionStorage.getItem("city");
 document.getElementById("email").innerText = sessionStorage.getItem("email");
+// Kurswahl:
+document.getElementById("FormControlSelect1").innerText = sessionStorage.getItem("FormcontrolSelect1");
+} 
 
+fillIn();
